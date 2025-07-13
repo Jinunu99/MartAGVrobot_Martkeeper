caa95b2bb2da58d99b6f3fcd632e54f2b3547870
hyunsoo-Oh <hyunsoo2245@gmail.com>
2025-07-13 15:27:59 +0900

    hyunsoo-Oh/Create_STM

---
+++ stm_controller/App/hw/hw.c
@@ -0,0 +1,13 @@
+/*
+ * ap.c
+ *
+ *  Created on: Jul 12, 2025
+ *      Author: User
+ */
+
+#include "hw.h"
+
+void hwInit(void)
+{
+
+}
---
+++ stm_controller/App/hw/hw.h
@@ -0,0 +1,15 @@
+/*
+ * hw.h
+ *
+ *  Created on: Jul 12, 2025
+ *      Author: User
+ */
+
+#ifndef HW_HW_H_
+#define HW_HW_H_
+
+#include "def.h"
+
+void hwInit(void);
+
+#endif /* HW_HW_H_ */
