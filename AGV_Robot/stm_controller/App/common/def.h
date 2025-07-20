ebc0a7ee51cb48346ba46c3b91f7fd2ae287bc38
Jinunu99 <jinwoo7558@gmail.com>
2025-07-20 16:35:01 +0900

    manager_commit_first

---
+++ AGV_Robot/stm_controller/App/common/def.h
@@ -0,0 +1,16 @@
+/*
+ * def.h
+ *
+ *  Created on: Jul 12, 2025
+ *      Author: User
+ */
+
+#ifndef COMMON_DEF_H_
+#define COMMON_DEF_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "string.h"
+
+#endif /* COMMON_DEF_H_ */
