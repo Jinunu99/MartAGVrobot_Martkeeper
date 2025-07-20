2c35bdc3c03d205a315c02025233ee8c2476f047
Jinunu99 <jinwoo7558@gmail.com>
2025-07-20 16:35:01 +0900

    manager_commit_first

---
+++ AGV_Robot/stm_controller/App/devices/serial/serial.c
@@ -0,0 +1,73 @@
+/*
+ * serial.c
+ *
+ *  Created on: Jul 12, 2025
+ *      Author: User
+ */
+
+#include "def.h"
+#include "usart.h"
+#include "serial.h"
+
+#define DATA_SIZE		64
+#define DMA_SIZE		64
+
+volatile uint8_t rxFlag = 0;                // RX 완료 플래그
+volatile uint8_t txFlag = 0;                // TX 완료 플래그
+
+static uint8_t rxBuff[DMA_SIZE];
+static uint8_t rxData[DATA_SIZE];           // 외부에서 읽어갈 수 있는 RX 데이터 버퍼
+static uint8_t rxLength;
+static uint8_t txData[DATA_SIZE];           // TX 송신 데이터 버퍼
+
+void SERIAL_Init(void)
+{
+	HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rxBuff, DMA_SIZE); 	  // DMA + IDLE로 UART 수신 시작
+	__HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);				  // Half-transfer interrupt disable
+	__HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);				  // IDLE interrupt enable
+}
+
+// 외부에서 송신할 데이터를 txData에 복사
+void SERIAL_PutData(uint8_t* buff)
+{
+	memcpy(txData, buff, sizeof(txData));
+}
+
+// 외부에서 rxData 내용을 접근할 수 있도록 buff에 복사
+void SERIAL_GetData(uint8_t* buff)
+{
+	snprintf((char*)buff, DATA_SIZE, "%s\r\n", rxData);
+}
+
+void SERIAL_Print(void)
+{
+	if (txFlag == 1)
+	{
+//		HAL_UART_Transmit(&huart2, txData, strlen((char*)txData), 100);
+		HAL_UART_Transmit_DMA(&huart1, txData, strlen((char*)txData));
+	}
+}
+
+void SERIAL_Scanf(uint16_t Size)
+{
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        char c = (char)rxBuff[i];
+
+        if (c != '\n' && rxLength < DATA_SIZE)
+        {
+        	rxData[rxLength++] = c;
+        }
+        else if (c == '\n')
+        {
+        	rxData[rxLength] = '\0';
+        	rxLength = 0;
+        	rxFlag = 1;
+        }
+    }
+}
+
+void SERIAL_Flag(void)
+{
+	txFlag = 0;
+}
--- model/Model_README.md
+++ model/Model_README.md
@@ -1,12 +1,55 @@
+<!-- # snack_dataset url 
+# https://universe.roboflow.com/korea-nazarene-university/-d9kpq/dataset/3
 
+# /home/paper/workspace/MartAGVrobot_Martkeeper/agvenv/bin/python3.9 -m pip install --upgrade pip
 
 # requirements 설치 명령어
 # pip install -r requirements.txt
 
-# snack_dataset url 
-# https://universe.roboflow.com/korea-nazarene-university/-d9kpq/dataset/3
+# 파이썬 3.9로 가상환경 생성
+# /usr/local/bin/python3.9 -m venv agvenv
 
-# /home/paper/workspace/MartAGVrobot_Martkeeper/agvenv/bin/python3.9 -m pip install --upgrade pip
+# liblzma-dev 설치
+<!-- sudo apt update -->
+<!-- sudo apt install liblzma-dev -->
+
+<!-- find ~ -type d -name "Python-3.9*" 로 경로 확인 후 이동, 재빌드
+
+make clean
+./configure --enable-optimizations
+make -j$(nproc)
+sudo make altinstall -->
+
+
+
+<!-- # bz2 설치
+sudo apt update
+sudo apt install libbz2-dev
+
+# cd ~/Python-3.9.2 이동
+# 빌드 --> 
+
+<!-- make clean
+./configure --enable-optimizations
+make -j$(nproc)
+sudo make altinstall -->
+
+<!-- /usr/local/bin/python3.9 -c "import bz2; print('✅ bz2 정상 작동')" -->
+
+<!-- cd ~/workspace/MartAGVrobot_Martkeeper
+rm -rf agvenv
+/usr/local/bin/python3.9 -m venv agvenv
+source agvenv/bin/activate
+python -c "import bz2; print('✅ 가상환경에서 bz2 정상 작동')" -->
+
+
+<!-- # pip upgade 
+# python -m pip install --upgrade pip
+
+# MariDB 실행 / 허용
+# sudo systemctl start mariadb.service
+# sudo systemctl enable mariadb.service -->
+
 
 # Tree
 
